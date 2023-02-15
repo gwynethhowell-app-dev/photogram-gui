93cd5293130c4e5fe64170e0631b630bf21fab64
gwynethhowell <ghowell@uchicago.edu>
2023-02-15 18:15:49 +0000

    user details page works

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,46 @@
 class UsersController < ApplicationController
 
-  def index
-    matching_users = User.all
-
-    @list_of_users = matching_users.order({ :username => :asc })
-    render({ :template => "user_templates/index.html.erb"})
+    def index
+  
+      matching_users = User.all
+      @list_of_users = matching_users.order({ :username => :asc})
+  
+      render({ :template => "user_templates/index.html.erb"})
+    end
+  
+    def details 
+      url_username = params.fetch("user_id")
+      matching_users = User.where({ :username => url_username})
+      @the_user = matching_users.at(0)
+      render({ :template => "user_templates/show_details.html.erb"})
+    end 
+  
+    def add_user
+      input_username = params.fetch("input_username")
+  
+      new_user = User.new
+      new_user.username = input_username
+      new_user.save
+  
+      redirect_to( "/users/" + input_username)
+    end
+  
+    def update
+      the_id = params.fetch("modify_id")
+      matching_user = User.where({ :id => the_id})
+      the_user_update = matching_user.first
+  
+      updated_user = params.fetch("new_user")
+  
+      the_user_update.username = updated_user
+  
+      the_user_update.save
+  
+      redirect_to( "/users/" + the_user_update.username )
+    end
+  
+    def home
+      redirect_to( "/users")
+    end
+  
   end
-end 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
 
-get("/users", {:controller => "users", :action => "index"})
+  get("/", { :controller => "users", :action => "home"})
+
+  get("/users", { :controller => "users", :action => "index"})
+  get("/users/:user_id", { :controller => "users", :action => "details"})
+  get("/add_user", { :controller => "users", :action => "add_user"})
+  get("/update_user/:modify_id", { :controller => "users", :action => "update"})
+
+  get("/photos", { :controller => "photos", :action => ""})
 
 end
